874d54d3be6e34765130adfa01d04987645e7df8
Damian Sloane <dddaaammmooo@hotmail.com>
2019-11-29 08:33:11 +1100

    Wildcard logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 # Get parameters from Github Actions workflow
-TEMPLATE_ROOT=$(echo ${1} | tr -s /)
-CONFIG_ROOT=$(echo ${2} | tr -s /)
-OUTPUT_PATH=$(echo ${3} | tr -s /)
-OUTPUT_FILENAME_PREFIX=${4}
-OUTPUT_FILENAME_SUFFIX=${5}
+ENVIRONMENT=$(echo ${1})
+TEMPLATE_ROOT=$(echo ${2} | tr -s /)
+CONFIG_ROOT=$(echo ${3} | tr -s /)
+OUTPUT_PATH=$(echo ${4} | tr -s /)
+OUTPUT_FILENAME_PREFIX=${5}
+OUTPUT_FILENAME_SUFFIX=${6}
 
 # Validate all paths specified can be found
 if [[ ! -d "${TEMPLATE_ROOT}" ]]; then
@@ -24,18 +25,35 @@ fi
 # Install Python requirements
 pip install -r ./requirements.txt
 
-# Iterate over each environment folder
-for ENVIRONMENT_CURRENT in ${CONFIG_ROOT}/* ; do
-    if [[ -d "${ENVIRONMENT_CURRENT}" ]]; then
-        ENVIRONMENT_CODE=$(basename ${ENVIRONMENT_CURRENT})
-        OUTPUT_FILENAME=$(echo "./${OUTPUT_PATH}/${OUTPUT_FILENAME_PREFIX}${ENVIRONMENT_CODE}${OUTPUT_FILENAME_SUFFIX}" | tr -s /)
+if [[ "${ENVIRONMENT}" != "*" ]]; then
 
-        # Build the template
-        echo "Building: ${OUTPUT_FILENAME}"
-        python template.py \
-          "${ENVIRONMENT_CODE}" \
-          "${TEMPLATE_ROOT}" \
-          "${CONFIG_ROOT}" \
-          "${OUTPUT_FILENAME}"
-    fi
-done
+  # Building a single environment from the config root folder
+  OUTPUT_FILENAME=$(echo "./${OUTPUT_PATH}/${OUTPUT_FILENAME_PREFIX}${ENVIRONMENT}${OUTPUT_FILENAME_SUFFIX}" | tr -s /)
+
+  # Build the template
+  echo "Building: ${OUTPUT_FILENAME}"
+  python template.py \
+    "${ENVIRONMENT}" \
+    "${TEMPLATE_ROOT}" \
+    "${CONFIG_ROOT}" \
+    "${OUTPUT_FILENAME}"
+
+else
+
+  # Building all environments in the config root folder
+  for ENVIRONMENT_CURRENT in ${CONFIG_ROOT}/* ; do
+      if [[ -d "${ENVIRONMENT_CURRENT}" ]]; then
+          ENVIRONMENT_CURRENT=$(basename ${ENVIRONMENT_CURRENT})
+          OUTPUT_FILENAME=$(echo "./${OUTPUT_PATH}/${OUTPUT_FILENAME_PREFIX}${ENVIRONMENT_CODE}${OUTPUT_FILENAME_SUFFIX}" | tr -s /)
+
+          # Build the template
+          echo "Building: ${OUTPUT_FILENAME}"
+          python template.py \
+            "${ENVIRONMENT_CURRENT}" \
+            "${TEMPLATE_ROOT}" \
+            "${CONFIG_ROOT}" \
+            "${OUTPUT_FILENAME}"
+      fi
+  done
+
+fi
